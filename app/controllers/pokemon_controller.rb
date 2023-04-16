e3f3bc6a87c093cfe5f230dc12048ef36a667d42
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-04-16 11:06:53 +0300

    hard-coded call to the api

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+require 'json'
 class PokemonController < ApplicationController
   def index
-
+    conn = Faraday.new('https://pokeapi.co/')
+    response = conn.get('/api/v2/pokemon-form/150')
+    @pokemon = JSON.parse(response.body, symbolize_names: true)
+    binding.irb
   end
 end
